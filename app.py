f70e6fd2e962b3a3947ba3af9c1776255c6d50ea
황지훈 <rkgh17@gmail.com>
2022-11-11 17:17:10 +0900

    cal

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, request
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.by import By
 import time
 import json
 
