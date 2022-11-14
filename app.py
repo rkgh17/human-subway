06b1bd7771e816a2fe95503424d2157a78f7f3a5
황지훈 <rkgh17@gmail.com>
2022-11-14 10:51:38 +0900

    sub

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
