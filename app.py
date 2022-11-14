484f8bfb8fa42485d7f650d6f128451000273ab4
황지훈 <rkgh17@gmail.com>
2022-11-14 10:39:56 +0900

    sub

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
 from flask import Flask, request
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -15,6 +14,18 @@ chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+driver.get("https://safecity.seoul.go.kr/acdnt/sbwyIndex.do")
+time.sleep(0.5)
+
+parentElement = driver.find_elements(By.XPATH, '//*[@id="dv_as_timeline"]/li')
+subli=[]
+# ul 태그 아래 있는 li 반복 뽑기
+for i in parentElement:
+    i.click()
+    time.sleep(0.05)
+    a = i.text
+    subli.append(a)
+    i.click()
 
 app = Flask(__name__)
 
@@ -121,19 +132,6 @@ def saysubway():
     body = request.get_json()
     print(body)
     print(body['userRequest']['utterance'])
-
-    driver.get("https://safecity.seoul.go.kr/acdnt/sbwyIndex.do")
-    time.sleep(0.5)
-
-    parentElement = driver.find_elements(By.XPATH, '//*[@id="dv_as_timeline"]/li')
-    subli=[]
-    # ul 태그 아래 있는 li 반복 뽑기
-    for i in parentElement:
-        i.click()
-        time.sleep(0.05)
-        a = i.text
-        subli.append(a)
-        i.click()
 
     responseBody = {
         "version": "2.0",
