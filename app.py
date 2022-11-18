241d2c082e4d219a6e3282b62281157623576839
hjh_home <rkgh17@gmail.com>
2022-11-18 23:13:27 +0900

    newssearch

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 def index():
     return "hello"
 
-## 오늘의 사고내역 보내주기
+## 오늘의 지하철 속보
 @app.route('/api/saysubway', methods=['POST'])
 def saysubway():
     body = request.get_json()
@@ -60,7 +60,7 @@ def saysubway():
     }
     return responseBody
 
-# 경로 url 전달 함수
+# 경로 url 변환 함수
 def location(searching):
     url = 'https://dapi.kakao.com/v2/local/search/keyword.json?query={}'.format(searching)
     headers = {
@@ -70,7 +70,7 @@ def location(searching):
     
     return destination
 
-# 경로찾기 응답
+# 목적지 검색
 @app.route('/api/goto', methods=['POST'])
 def goto():
     body = request.get_json()
@@ -87,6 +87,71 @@ def goto():
                 {
                     "simpleText": {
                         "text": answer_text
+                    }
+                }
+            ]
+        }
+    }
+
+    return responseBody
+
+
+# 뉴스 검색
+@app.route('/api/newslist', methods=['POST'])
+def newslist():
+    body = request.get_json()
+    print(body)
+    hosun = (body['action']['params'])['sys_news']
+
+    conn = psycopg2.connect(host="ec2-23-21-207-93.compute-1.amazonaws.com", 
+                            dbname="d3oubpekvnbupv", 
+                            user="grxhirqndvyqvv", 
+                            password="6f1afaafe16d245c70666bdb8c831aa876e62b380a1544f5dc832c51f27cece6", 
+                            port="5432")
+
+    cur = conn.cursor()
+
+    newhosun = '%' + hosun + '%'
+    cur.execute(f"SELECT * from newsdata WHERE newsname LIKE '{newhosun}' order by newsdate desc limit 5")
+    result_all = cur.fetchall()
+
+    newsstr = ""
+
+    for i in result_all:
+        for j in i:
+            newsstr = newsstr + j + '\n'
+        newsstr = newsstr + '\n'
+    newsstr = newsstr[:-2]
+
+    responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": newsstr
+                    }
+                }
+            ]
+        }
+    }
+    return responseBody
+
+# 간편 지연 증명서
+@app.route('/api/simpleDelay', methods=['POST'])
+def simpleDelay():
+
+    body = request.get_json()
+    print(body)
+    print(body['userRequest']['utterance'])
+
+    responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": "http://www.seoulmetro.co.kr/kr/delayProofList.do?menuIdx=543"
                     }
                 }
             ]
@@ -123,69 +188,77 @@ def sayHello():
 
     return responseBody
 
-## 카카오톡 이미지형 응답
-@app.route('/api/showHello', methods=['POST'])
-def showHello():
-    body = request.get_json()
-    print(body)
-    print(body['userRequest']['utterance'])
 
-    responseBody = {
-        "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "simpleImage": {
-                        "imageUrl": "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
-                        "altText": "hello I'm Ryan"
-                    }
-                }
-            ]
-        }
-    }
 
-    return responseBody
 
-## 메인 로직!! 
-def cals(opt_operator, number01, number02):
-    if opt_operator == "addition":
-        return number01 + number02
-    elif opt_operator == "subtraction": 
-        return number01 - number02
-    elif opt_operator == "multiplication":
-        return number01 * number02
-    elif opt_operator == "division":
-        return number01 / number02
 
-## 카카오톡 Calculator 계산기 응답
-@app.route('/api/calCulator', methods=['POST'])
-def calCulator():
-    body = request.get_json()
-    print(body)
-    params_df = body['action']['params']
-    print(type(params_df))
-    opt_operator = params_df['operators']
-    number01 = json.loads(params_df['sys_number01'])['amount']
-    number02 = json.loads(params_df['sys_number02'])['amount']
 
-    print(opt_operator, type(opt_operator), number01, type(number01))
 
-    answer_text = str(cals(opt_operator, number01, number02))
 
-    responseBody = {
-        "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "simpleText": {
-                        "text": answer_text
-                    }
-                }
-            ]
-        }
-    }
 
-    return responseBody
+# ## 카카오톡 이미지형 응답
+# @app.route('/api/showHello', methods=['POST'])
+# def showHello():
+#     body = request.get_json()
+#     print(body)
+#     print(body['userRequest']['utterance'])
+
+#     responseBody = {
+#         "version": "2.0",
+#         "template": {
+#             "outputs": [
+#                 {
+#                     "simpleImage": {
+#                         "imageUrl": "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
+#                         "altText": "hello I'm Ryan"
+#                     }
+#                 }
+#             ]
+#         }
+#     }
+
+#     return responseBody
+
+# ## 메인 로직!! 
+# def cals(opt_operator, number01, number02):
+#     if opt_operator == "addition":
+#         return number01 + number02
+#     elif opt_operator == "subtraction": 
+#         return number01 - number02
+#     elif opt_operator == "multiplication":
+#         return number01 * number02
+#     elif opt_operator == "division":
+#         return number01 / number02
+
+# ## 카카오톡 Calculator 계산기 응답
+# @app.route('/api/calCulator', methods=['POST'])
+# def calCulator():
+#     body = request.get_json()
+#     print(body)
+#     params_df = body['action']['params']
+#     print(type(params_df))
+#     opt_operator = params_df['operators']
+#     number01 = json.loads(params_df['sys_number01'])['amount']
+#     number02 = json.loads(params_df['sys_number02'])['amount']
+
+#     print(opt_operator, type(opt_operator), number01, type(number01))
+
+#     answer_text = str(cals(opt_operator, number01, number02))
+
+#     responseBody = {
+#         "version": "2.0",
+#         "template": {
+#             "outputs": [
+#                 {
+#                     "simpleText": {
+#                         "text": answer_text
+#                     }
+#                 }
+#             ]
+#         }
+#     }
+
+#     return responseBody
 
 
 
